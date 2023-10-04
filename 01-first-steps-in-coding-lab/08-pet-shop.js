654ac93840f04f2362acac27bbb38614615e3de8
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-04 08:52:49 +0300

    Add solution to eighth task.

---
+++ 01-first-steps-in-coding-lab/08-pet-shop.js
@@ -0,0 +1,10 @@
+function petShop(input) {
+    let dogsNumber = Number(input[0]);
+    let otherAnimals = Number(input[1]);
+
+    let dogsFoodAmount = dogsNumber * 2.50;
+    let otherAnimalsAmount = otherAnimals * 4;
+
+    let totalAmount = dogsFoodAmount + otherAnimalsAmount;
+    console.log(`${totalAmount} lv.`);
+}
