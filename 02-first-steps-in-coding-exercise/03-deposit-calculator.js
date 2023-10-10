dca3cc1d76c00cfec7835a2a9fbcc2687ca1e2c5
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-10 10:39:05 +0300

    Add solution to third task.

---
+++ 02-first-steps-in-coding-exercise/03-deposit-calculator.js
@@ -0,0 +1,11 @@
+function depositCalculator(input) {
+    let depositAmount = Number(input[0]);
+    let depositTerm = Number(input[1]);
+    let yearPercent = Number(input[2]);
+
+    let divident = depositAmount * (yearPercent / 100);
+    let dividentForMonth = divident / 12;
+    let sum = depositAmount + (depositTerm * dividentForMonth);
+
+    console.log(sum);
+}
