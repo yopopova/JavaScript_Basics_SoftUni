9a9da29af93a587e99b812cdbd2a7550201e390c
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-13 12:10:06 +0300

    Add solution to sixth task.

---
+++ 02-first-steps-in-coding-exercise/06-charity-campaign.js
@@ -0,0 +1,15 @@
+function charity(input) {
+    let days = Number(input[0]);
+    let cooks = Number(input[1]);
+    let cakes = Number(input[2]);
+    let waffles = Number(input[3]);
+    let pancakes = Number(input[4]);
+
+    let cakeAmount = cakes * cooks * days * 45;
+    let wafflesAmount = waffles * cooks * days * 5.80;
+    let pancakesAmount = pancakes * cooks * days * 3.20;
+
+    let result = cakeAmount + wafflesAmount + pancakesAmount;
+    result -= 1/8 * result;
+    console.log(result);
+}
