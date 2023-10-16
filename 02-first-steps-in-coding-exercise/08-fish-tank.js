ef60fedff6520b393ae0c0d14ef2310e3133cb25
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-16 17:11:19 +0300

    Add solution to eighth task.

---
+++ 02-first-steps-in-coding-exercise/08-fish-tank.js
@@ -0,0 +1,13 @@
+function fishTank(input) {
+    let lengthCm = Number(input[0]);
+    let widthCm = Number(input[1]);
+    let heightCm = Number(input[2]);
+    let percent = Number(input[3]);
+
+    let tankVolume = lengthCm * widthCm * heightCm;
+    let tankLiters = tankVolume * 0.001;
+    let sandPercent = percent * 0.01;
+
+    neededLiters = tankLiters * (1 - sandPercent);
+    console.log(neededLiters);
+}
