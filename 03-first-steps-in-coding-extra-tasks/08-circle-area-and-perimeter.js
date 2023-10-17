50e3b6afe6243d464150f06e98c36e934e94b1e2
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-17 08:58:17 +0300

    Add solution to eighth task.

---
+++ 03-first-steps-in-coding-extra-tasks/08-circle-area-and-perimeter.js
@@ -0,0 +1,9 @@
+function circleAreaAndPerimeter(input) {
+    let r = Number(input[0]);
+
+    let circleFace = Math.PI * r * r;
+    let circlePerimeter = 2 * Math.PI * r;
+
+    console.log(circleFace.toFixed(2));
+    console.log(circlePerimeter.toFixed(2));
+}
