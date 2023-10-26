aad8743a49877683f5fb3d3b7578bccc6ce6a18a
Yoanna Popova <yoanna.dp@gmail.com>
2023-10-26 12:33:44 +0300

    Add solution to fourth task.

---
+++ 04-conditional-statements-lab/04-number-between-100-and-200.js
@@ -0,0 +1,11 @@
+function numberBetween(input) {
+    let num = Number(input[0]);
+
+    if(num < 100) {
+        console.log("Less than 100");
+    } else if(num <= 200) {
+        console.log("Between 100 and 200");
+    } else if(num > 200) {
+        console.log("Greater than 200");
+    }
+}
