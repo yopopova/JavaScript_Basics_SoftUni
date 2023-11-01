7eee7a9e0ebceecce91031be6481ee1f0a3dd152
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-01 09:08:34 +0200

    Add solution to seventh task.

---
+++ 05-conditional-statements-exercise/07-world-swimming-record.js
@@ -0,0 +1,17 @@
+function worldSwimmingRecord(input) {
+    let recordInSec = Number(input[0]);
+    let distanceInM = Number(input[1]);
+    let timeInSecForMeter = Number(input[2]);
+
+    let slow = Math.floor(distanceInM / 15);
+
+    let result = distanceInM * timeInSecForMeter;
+    result = result + (slow * 12.5);
+
+    if(result < recordInSec) {
+        console.log(`Yes, he succeeded! The new world record is ${result.toFixed(2)} seconds.`);
+    } else {
+        let diff = Math.abs(result - recordInSec);
+        console.log(`No, he failed! He was ${diff.toFixed(2)} seconds slower.`);
+    }
+}
