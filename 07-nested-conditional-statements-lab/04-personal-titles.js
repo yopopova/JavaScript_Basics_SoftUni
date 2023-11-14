acd3c49a0e4a521d2f2e954f04022ba56b9e6418
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-14 09:54:09 +0200

    Add solution to fourth task.

---
+++ 07-nested-conditional-statements-lab/04-personal-titles.js
@@ -0,0 +1,26 @@
+function personalTitles(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    if(gender === "f") {
+
+        if(age < 16) {
+            console.log("Miss");
+        } else {
+            console.log("Ms.");
+        }
+
+    } else if(gender === "m") {
+
+        if(age < 16) {
+            console.log("Master");
+        } else {
+            console.log("Mr.");
+        }
+    }
+}
+
+personalTitles(["12", "f"]);
+personalTitles(["17", "m"]);
+personalTitles(["25", "f"]);
+personalTitles(["13.5", "m"]);
