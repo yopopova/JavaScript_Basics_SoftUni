5522ba17081745ffc4eec36c8d351d6edde755e1
Yoanna Popova <yoanna.dp@gmail.com>
2023-11-30 22:39:39 +0200

    Add solution to fourth task.

---
+++ 08-nested-conditional-statements-exercise/04-fishing-boat.js
@@ -0,0 +1,61 @@
+function fishingBoat(input) {
+    let budget = Number(input[0]);
+    let season = input[1];
+    let countFisherman = Number(input[2]);
+
+    let needMoney = 0;
+
+    switch(season) {
+        case "Spring":
+            needMoney = 3000;
+
+            if(countFisherman <= 6) {
+                needMoney = needMoney * 0.90;
+            } else if(countFisherman >= 7 && countFisherman <= 11) {
+                needMoney = needMoney * 0.85;
+            } else {
+                needMoney = needMoney * 0.75;
+            }
+            break;
+        case "Summer":
+        case "Autumn":
+            needMoney = 4200;
+
+            if(countFisherman <= 6) {
+                needMoney = needMoney * 0.90;
+            } else if(countFisherman >= 7 && countFisherman <= 11) {
+                needMoney = needMoney * 0.85;
+            } else {
+                needMoney = needMoney * 0.75;
+            }
+            break;
+        case "Winter":
+            needMoney = 2600;
+
+            if(countFisherman <= 6) {
+                needMoney = needMoney * 0.90;
+            } else if(countFisherman >= 7 && countFisherman <= 11) {
+                needMoney = needMoney * 0.85;
+            } else {
+                needMoney = needMoney * 0.75;
+            }
+            break;
+    }
+
+    if(countFisherman % 2 === 0 && season != "Autumn") {
+        needMoney = needMoney * 0.95;
+    }
+
+    let diff = Math.abs(budget - needMoney);
+
+    if(budget >= needMoney) {
+        console.log(`Yes! You have ${diff.toFixed(2)} leva left.`);
+    } else {
+        console.log(`Not enough money! You need ${diff.toFixed(2)} leva.`);
+    }
+    
+}
+
+fishingBoat(["3000", "Summer", "11"]);
+fishingBoat(["3600", "Autumn", "6"]);
+fishingBoat(["2000", "Winter", "13"]);
