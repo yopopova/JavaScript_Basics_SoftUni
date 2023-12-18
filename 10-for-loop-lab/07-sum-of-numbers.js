cc1fe01e5e1f5b025f3d7f1ea13b0bbd8021890e
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-18 18:12:10 +0200

    Add solution to seventh task.

---
+++ 10-for-loop-lab/07-sum-of-numbers.js
@@ -0,0 +1,16 @@
+function sumOfNumbers(input) {
+    let textNumber = input[0];
+    let textLength = textNumber.length;
+
+    let sum = 0;
+
+    for(let i = 0; i < textLength; i++) {
+        let currentNumber = Number(textNumber[i]);
+        sum += currentNumber;
+    }
+
+    console.log(`The sum of the digits is:${sum}`);
+}
+
+sumOfNumbers(["1234"]);
+sumOfNumbers(["564891"]);
