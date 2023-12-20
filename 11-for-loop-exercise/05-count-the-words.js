553b72511d0c806a27e83e697bf9f0c72e28fa8a
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-20 15:40:07 +0200

    Add solution to fifth task.

---
+++ 11-for-loop-exercise/05-count-the-words.js
@@ -0,0 +1,19 @@
+function countTheWords(input) {
+    let text = input[0];
+    let counter = 1;
+
+    for(let i = 0; i < text.length; i++) {
+        if(text[i] === " ") {
+            counter++;
+        }
+    }
+
+    if(counter > 10) {
+        console.log(`The message is too long to be send! Has ${counter} words.`);
+    } else {
+        console.log("The message was sent successfully!");
+    }
+}
+
+countTheWords(["This message has exactly eleven words. One more as it's allowed!"]);
+countTheWords(["This message has ten words and you can send it!"]);
