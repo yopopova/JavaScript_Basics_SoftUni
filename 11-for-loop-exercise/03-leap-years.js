d981b6c4f1c496a460acd52861cd020b78af473f
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-20 15:39:26 +0200

    Add solution to third task.

---
+++ 11-for-loop-exercise/03-leap-years.js
@@ -0,0 +1,13 @@
+function leapYears(input) {
+    let firstYear = Number(input[0]);
+    let secondYear = Number(input[1]);
+
+    for(let year = firstYear; year <= secondYear; year += 4) {
+        console.log(year);
+    }  
+}
+
+leapYears(["1908", "1919"]);
+// leapYears(["2000", "2011"]);
+// leapYears(["1584", "1597"]);
+// leapYears(["2020", "2032"]);
