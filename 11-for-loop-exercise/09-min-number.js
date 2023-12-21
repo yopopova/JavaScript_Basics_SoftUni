e1619061801bc14323b32a60fbf234331089b037
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-21 18:05:57 +0200

    Add solution to ninth task.

---
+++ 11-for-loop-exercise/09-min-number.js
@@ -0,0 +1,20 @@
+function minNumber(input) {
+    let index = 0;
+    let n = Number(input[index]);
+    index++;
+
+    let min = Number.MAX_SAFE_INTEGER;
+
+    for(let i = 0; i < n; i++) {
+        let num = Number(input[index]);
+        index++;
+
+        if(min > num) {
+            min = num;
+        } 
+    }
+
+    console.log(min);
+}
+
+minNumber(["2", "100", "99"]);
