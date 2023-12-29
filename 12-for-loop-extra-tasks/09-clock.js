9c9e7a4d8f3ea80b95b4664534699d39ee25fa06
Yoanna Popova <yoanna.dp@gmail.com>
2023-12-29 20:17:21 +0200

    Add solution to ninth task.

---
+++ 12-for-loop-extra-tasks/09-clock.js
@@ -0,0 +1,10 @@
+function clock() {
+
+    for(let hours = 0; hours <= 23; hours++) {
+        for(let mins = 0; mins <= 59; mins++) {
+            console.log(`${hours} : ${mins}`);
+        }
+    }
+}
+
+clock();
