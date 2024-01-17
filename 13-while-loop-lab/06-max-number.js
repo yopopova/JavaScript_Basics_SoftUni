c5e3d3fb1297919cde8382fcab582409a8695d93
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-17 11:37:42 +0200

    Add solution to sixth task.

---
+++ 13-while-loop-lab/06-max-number.js
@@ -0,0 +1,26 @@
+function maxNumber(input) {
+    let maxNum = Number.MIN_SAFE_INTEGER;
+
+    let index = 0;
+    let command = input[index];
+    index++;
+
+    while(command !== "Stop") {
+        let num = Number(command);
+
+        if(num > maxNum) {
+            maxNum = num;
+        }
+
+        command = input[index];
+        index++;
+    }
+
+    console.log(maxNum);
+}
+
+maxNumber(["100", "99", "80", "70", "Stop"]);
+maxNumber(["-10", "20", "-30", "Stop"]);
+maxNumber(["45", "-20", "7", "99", "Stop"]);
+maxNumber(["999", "Stop"]);
+maxNumber(["-1", "-2", "Stop"]);
