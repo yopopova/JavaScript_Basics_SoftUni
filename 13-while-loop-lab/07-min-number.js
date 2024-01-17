068c18377f8dfb71939372f03cf4e4d10fafd081
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-17 11:38:26 +0200

    Add solution to seventh task.

---
+++ 13-while-loop-lab/07-min-number.js
@@ -0,0 +1,26 @@
+function minNumber(input) {
+    let minNum = Number.MAX_SAFE_INTEGER;
+
+    let index = 0;
+    let command = input[index];
+    index++;
+
+    while(command !== "Stop") {
+        let num = Number(command);
+
+        if(num < minNum) {
+            minNum = num;
+        }
+
+        command = input[index];
+        index++;
+    }
+
+    console.log(minNum);
+}
+
+minNumber(["100", "99", "80", "70", "Stop"]);
+minNumber(["-10", "20", "-30", "Stop"]);
+minNumber(["45", "-20", "7", "99", "Stop"]);
+minNumber(["999", "Stop"]);
+minNumber(["-1", "-2", "Stop"]);
