a871be633f5fcde41e1880c17da013321c725675
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-19 11:26:41 +0200

    Add second solution to third task.

---
+++ 14-while-loop-exercise/03-vacation-v2.js
@@ -0,0 +1,50 @@
+function vacation(input) {
+    let index = 0;
+    let neededMoney = Number(input[index]);
+    index++;
+
+    let money = Number(input[index]);
+    index++;
+
+    let counter = 0;
+    let dayCounter = 0;
+
+    let isCantSaveMoney = false;
+
+    while(money < neededMoney) {
+        dayCounter++;
+
+        let action = input[index];
+        index++;
+
+        let currentMoney = Number(input[index]);
+        index++;
+
+        if(action === "spend") {
+            money -= currentMoney;
+
+            if(money < 0) {
+                money = 0;
+            }
+
+            counter++;
+        } else {
+            money += currentMoney;
+            counter = 0;
+        }
+
+        if(counter === 5) {
+            isCantSaveMoney = true;
+            console.log("You can't save the money.");
+            console.log(`${dayCounter}`);
+            break;
+        }
+    }
+
+    if(!isCantSaveMoney) {
+        console.log(`You saved the money for ${dayCounter} days.`);
+    }
+}
+
+vacation(["2000", "1000", "spend", "1200", "save", "2000"]);
+vacation(["110", "60", "spend", "10", "spend", "10", "spend", "10", "spend", "10", "spend", "10"]);
