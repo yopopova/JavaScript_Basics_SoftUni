59e66b4f68dff2c72a2de374dcb3e30770928577
Yoanna Popova <yoanna.dp@gmail.com>
2024-01-30 09:07:22 +0200

    Add fourth solution to second task.

---
+++ 16-nested-loops-lab/02-multiplication-table-v4.js
@@ -0,0 +1,15 @@
+function multiplicationTable() {
+
+    for(let i = 1; i <= 3; i++) {
+        for(let j = 1; j <= 10; j++) {
+
+            if(j === 5 && i === 2) {
+                continue;
+            }
+
+            console.log(`${i} * ${j} = ${i * j}`);
+        }
+    }
+}
+
+multiplicationTable();
