5f21e5bca36c34eb9ac6023fe0cb7f332e4e88f3
Yoanna Popova <yoanna.dp@gmail.com>
2024-02-01 10:36:53 +0200

    Add solution to eighth task.

---
+++ 16-nested-loops-lab/08-tournament-of-christmas.js
@@ -0,0 +1,58 @@
+function tournamentOfChristmas(input) {
+    let index = 0;
+    let days = Number(input[index]);
+    index++;
+
+    let winsCounter = 0;
+    let losesCounter = 0;
+
+    let donations = 0;
+
+    for (let i = 1; i <= days; i++) {
+        let command = input[index];
+        index++
+
+        let wins = 0;
+        let loses = 0;
+
+        let dayDonations = 0;
+
+        while (command !== "Finish") {
+            let result = input[index];
+            index++;
+
+            switch (result) {
+                case "win":
+                    dayDonations += 20;
+                    winsCounter++;
+                    wins++;
+                    break;
+                case "lose":
+                    losesCounter++;
+                    loses++;
+                    break;
+                case "Finish": break;
+            }
+
+            command = input[index];
+            index++;
+        }
+
+        if (wins > loses) {
+            dayDonations *= 1.1;
+            donations += dayDonations;
+        } else {
+            donations += dayDonations;
+        }
+    }
+
+    if (winsCounter > losesCounter) {
+        donations *= 1.2;
+        console.log(`You won the tournament! Total raised money: ${donations.toFixed(2)}`);
+    } else {
+        console.log(`You lost the tournament! Total raised money: ${donations.toFixed(2)}`);
+    }
+}
+
+tournamentOfChristmas(["2", "volleyball", "win", "football", "lose", "basketball", "win", "Finish", "golf", "win", "tennis", "win", "badminton", "win", "Finish"]);
+tournamentOfChristmas(["3", "darts", "lose", "handball", "lose", "judo", "win", "Finish", "snooker", "lose", "swimming", "lose", "squash", "lose", "table tennis", "win", "Finish", "volleyball", "win", "basketball", "win", "Finish"]);
