fc5155448600f944e4a4ac8d13bf4c52817bb5f7
Yoanna Popova <yoanna.dp@gmail.com>
2024-02-07 09:03:31 +0200

    Add solution to seventh task.

---
+++ 18-nested-loops-extra-tasks/07-safe-passwords-generator.js
@@ -0,0 +1,43 @@
+function safePasswordsGenerator(input) {
+    let num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+    let maxPassCount = Number(input[2]);
+
+    let symbol1 = 35;
+    let symbol2 = 64;
+
+    let passwordsCounter = 0;
+    let buffer = "";
+
+    for(let a = 1; a <= num1; a++) {
+        for(let b = 1; b <= num2; b++) {
+            let characterOne = String.fromCharCode(symbol1);
+            let characterTwo = String.fromCharCode(symbol2);
+
+            buffer += "" + characterOne + characterTwo + a + b + characterTwo + characterOne + "|";
+            passwordsCounter++;
+
+            if(passwordsCounter >= maxPassCount) {
+                console.log(buffer);
+                return;
+            }
+
+            symbol1++;
+
+            if(symbol1 > 55) {
+                symbol1 = 35;
+            }
+
+            symbol2++;
+
+            if(symbol2 > 96) {
+                symbol2 = 64;
+            }
+        }
+    }
+
+    console.log(buffer);
+}
+
+safePasswordsGenerator(["2", "3", "10"]);
+safePasswordsGenerator(["20", "50", "10"]);
