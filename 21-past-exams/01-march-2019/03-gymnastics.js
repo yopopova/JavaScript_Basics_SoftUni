aa85f636761ae464cc708fd730b9170114bacbf6
Yoanna Popova <yoanna.dp@gmail.com>
2024-02-20 09:41:56 +0200

    Add solution to third task.

---
+++ 21-past-exams/01-march-2019/03-gymnastics.js
@@ -0,0 +1,61 @@
+function gymnastics(input) {
+    let country = input[0];
+    let instrument = input[1];
+
+    let difficulty = 0;
+    let performance = 0;
+
+    if (country === "Russia") {
+
+        if (instrument === "ribbon") {
+            difficulty = 9.100;
+            performance = 9.400;
+        } else if (instrument === "hoop") {
+            difficulty = 9.300;
+            performance = 9.800;
+        } else if (instrument === "rope") {
+            difficulty = 9.600;
+            performance = 9.000;
+        }
+
+    } else if (country === "Bulgaria") {
+
+        if (instrument === "ribbon") {
+            difficulty = 9.600;
+            performance = 9.400;
+        } else if (instrument === "hoop") {
+            difficulty = 9.550;
+            performance = 9.750;
+        } else if (instrument === "rope") {
+            difficulty = 9.500;
+            performance = 9.400;
+        }
+
+    } else if (country === "Italy") {
+
+        if (instrument === "ribbon") {
+            difficulty = 9.200;
+            performance = 9.500;
+        } else if (instrument === "hoop") {
+            difficulty = 9.450;
+            performance = 9.350;
+        } else if (instrument === "rope") {
+            difficulty = 9.700;
+            performance = 9.150;
+        }
+
+    }
+
+    let totalScore = difficulty + performance;
+
+    let diff = 20 - totalScore;
+
+    let diffScorePecent = (diff / 20) * 100;
+
+    console.log(`The team of ${country} get ${totalScore.toFixed(3)} on ${instrument}.`);
+    console.log(`${diffScorePecent.toFixed(2)}%`);
+}
+
+gymnastics(["Bulgaria", "ribbon"]);
+gymnastics(["Russia", "rope"]);
+gymnastics(["Italy", "hoop"]);
