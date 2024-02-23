7edd66cf7713b7cdd21aeab1464af19bbe48099d
Yoanna Popova <yoanna.dp@gmail.com>
2024-02-23 15:02:43 +0200

    Add solution to second task.

---
+++ 21-past-exams/02-april-2019/02-godzilla-vs-kong.js
@@ -0,0 +1,28 @@
+function godzillaVsKong(input) {
+    let budget = Number(input[0]);
+    let count = Number(input[1]);
+    let price = Number(input[2]);
+
+    let decor = budget * 0.1;
+    let wearPrice = count * price;
+    let total = 0;
+
+    if(count > 150) {
+        wearPrice = wearPrice * 0.9;
+    }
+
+    total = decor + wearPrice;
+    let diff = Math.abs(total - budget);
+
+    if(total > budget) {
+        console.log("Not enough money!");
+        console.log(`Wingard needs ${diff.toFixed(2)} leva more.`)
+    } else {
+        console.log("Action!");
+        console.log(`Wingard starts filming with ${diff.toFixed(2)} leva left.`);
+    }
+}
+
+godzillaVsKong(["20000", "120", "55.5"]);
+godzillaVsKong(["15437.62", "186", "57.99"]);
+godzillaVsKong(["9587.88", "222", "55.68"]);
