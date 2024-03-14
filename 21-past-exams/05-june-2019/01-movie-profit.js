ce790f989e2d5f9f8d1a2eb768c69af25af44535
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-14 10:36:38 +0200

    Add solution to first task.

---
+++ 21-past-exams/05-june-2019/01-movie-profit.js
@@ -0,0 +1,21 @@
+function movieProfit(input) {
+    let movie = input[0];
+    let days = Number(input[1]);
+    let tickets = Number(input[2]);
+    let ticketPrice = Number(input[3]);
+    let cinemaPerc = Number(input[4]);
+
+    let totalTicketsPrice = tickets * ticketPrice;
+
+    let totalIncome = totalTicketsPrice * days;
+
+    let cinemaFee = totalIncome * cinemaPerc / 100;
+    let movieIncome = totalIncome - cinemaFee;
+
+    console.log(`The profit from the movie ${movie} is ${movieIncome.toFixed(2)} lv.`);
+}
+
+movieProfit(["The Programmer", "20", "500", "7.50", "7"]);
+movieProfit(["Python Basics", "40", "34785", "10.45", "14"]);
+movieProfit(["The Jungle", "22", "20500", "9.37", "30"]);
+movieProfit(["Sofia Day", "1", "100", "5", "5"]);
