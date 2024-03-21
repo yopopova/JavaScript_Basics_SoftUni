f5c7924ddbb1af11e5a10b58150a12ec421cba12
Yoanna Popova <yoanna.dp@gmail.com>
2024-03-21 11:03:45 +0200

    Add solution to fourth task.

---
+++ 21-past-exams/05-june-2019/04-movie-stars.js
@@ -0,0 +1,42 @@
+function movieStars(input) {
+    let index = 0;
+    let budget = Number(input[index]);
+    index++;
+
+    let command = input[index];
+    index++;
+
+    let actorSalary = 0;
+
+    while (command !== "ACTION") {
+        let actorName = command;
+        let actorNameLength = actorName.length;
+
+        if(actorNameLength > 15) {
+            actorSalary = budget * 0.2;
+        } else {
+            let money = Number(input[index]);
+            index++;
+
+            actorSalary = money;
+        }
+
+        budget -= actorSalary;
+
+        if(budget <= 0) {
+            budget = Math.abs(budget);
+            console.log(`We need ${budget.toFixed(2)} leva for our actors.`);
+            break;
+        }
+
+        command = input[index];
+        index++;
+    }
+
+    if(command === "ACTION") {
+        console.log(`We are left with ${budget.toFixed(2)} leva.`);
+    }
+}
+
+// movieStars(["90000", "Christian Bale", "70000.50", "Leonard DiCaprio", "Kevin Spacey", "24000.99"]);
+movieStars(["170000", "Ben Affleck", "40000.50", "Zahari Baharov", "80000", "Tom Hanks", "2000.99", "ACTION"]);
