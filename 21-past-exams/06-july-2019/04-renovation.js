7f816fdf3cc90a5fac1c208b9535f0f4c6425df8
Yoanna Popova <yoanna.dp@gmail.com>
2024-04-15 19:29:20 +0300

    Add solution to fourth task.

---
+++ 21-past-exams/06-july-2019/04-renovation.js
@@ -0,0 +1,45 @@
+function renovation(input) {
+    let index = 0;
+    let h = Number(input[index]);
+    index++;
+
+    let w = Number(input[index]);
+    index++;
+
+    let quota = Number(input[index]);
+    index++;
+
+    let percentages = quota / 100;
+    let totalArea = 0;
+    let areaForPainting = 0;
+    let litersCount = 0;
+
+    for(let i = 0; i < input.length - 1; i++) {
+        let paintLiters = input[index];
+        index++;
+
+        if(paintLiters !== "Tired!") {
+            paintLiters = Number(paintLiters);
+
+            totalArea = (h * w) * 4;
+            areaForPainting = Math.ceil(totalArea - (totalArea * percentages));
+            litersCount += paintLiters;
+
+            if(litersCount > areaForPainting) {
+                let diff = litersCount - areaForPainting;
+                console.log(`All walls are painted and you have ${diff} l paint left!`);
+                break;
+            } else if(litersCount === areaForPainting) {
+                console.log("All walls are painted! Great job, Pesho!");
+                break;
+            }
+
+        } else {
+            console.log(`${areaForPainting - litersCount} quadratic m left.`);
+            break;
+        }
+    }
+}
+
+renovation(["3", "5", "10", "2", "3", "4", "Tired!"]);
+renovation(["2", "3", "25", "6", "7", "8"]);
