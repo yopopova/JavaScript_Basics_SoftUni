00ee9964754a9bd055c90f4fc45637b5dd7b4e8d
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-10 15:53:48 +0300

    Add solution to fourth task.

---
+++ 21-past-exams/07-march-2020/04-trekking-mania.js
@@ -0,0 +1,46 @@
+function trekkingMania(input) {
+    let index = 0;
+    let allGroups = Number(input[index]);
+    index++;
+
+    let musala = 0;
+    let monblan = 0;
+    let kilimandjaro = 0;
+    let kTwo = 0;
+    let everest = 0;
+
+    let allPeople = 0;
+
+    for(let i = 0; i < allGroups; i++) {
+        let group = Number(input[index]);
+        index++;
+
+        if(group <= 5) {
+            musala += group;
+        } else if(group >= 6 && group <= 12) {
+            monblan += group;
+        } else if(group >= 13 && group <= 25) {
+            kilimandjaro += group;
+        } else if(group >= 26 && group <= 40) {
+            kTwo += group;
+        } else if(group >= 41) {
+            everest += group;
+        }
+
+        allPeople += group;
+    }
+
+    let groupMusala = (musala / allPeople) * 100;
+    let groupMonblan = (monblan / allPeople) * 100;
+    let groupKilimandjaro = (kilimandjaro / allPeople) * 100;
+    let groupKTwo = (kTwo / allPeople) * 100;
+    let groupEverest = (everest / allPeople) * 100;
+
+    console.log(`${groupMusala.toFixed(2)}%`);
+    console.log(`${groupMonblan.toFixed(2)}%`);
+    console.log(`${groupKilimandjaro.toFixed(2)}%`);
+    console.log(`${groupKTwo.toFixed(2)}%`);
+    console.log(`${groupEverest.toFixed(2)}%`);
+}
+
+trekkingMania(["10", "10", "5", "1", "100", "12", "26", "17", "37", "40", "78"]);
